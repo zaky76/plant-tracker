44ca8f2f8c10b6c69da802ca88e9005d0ad45771
Zaky Hossain <zaky15-3610@diu.edu.bd>
2026-02-24 01:22:32 +0600

    plan tracker project almost done without delete button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let thrivlingCountList = [];
 let strugglingCountList = [];
+let currentStatus = 'all'
 
 let totalCount = document.getElementById('totalCount');
 let thrivlingCount = document.getElementById('thrivlingCount');
@@ -14,7 +15,7 @@ const strugglingFilterBtn = document.getElementById('struggling-filter-btn');
 const filteredSection = document.getElementById('filtered-section');
 
 
-function calculateTotalCount (){
+function calculateTotalCount() {
     totalCount.innerText = allCardSection.children.length
     thrivlingCount.innerText = thrivlingCountList.length
     strugglingCount.innerText = strugglingCountList.length
@@ -24,68 +25,136 @@ calculateTotalCount()
 
 
 //toggle (When select any button then selected button bg black and text white and other button will bg gray)
-function toggleStyle(id){
-   allFilterBtn.classList.remove('bg-black','text-white')
-   thrivingFilterBtn.classList.remove('bg-black', 'text-white')
-   strugglingFilterBtn.classList.remove('bg-black','text-white')
+function toggleStyle(id) {
+    allFilterBtn.classList.remove('bg-black', 'text-white')
+    thrivingFilterBtn.classList.remove('bg-black', 'text-white')
+    strugglingFilterBtn.classList.remove('bg-black', 'text-white')
 
-   allFilterBtn.classList.add('bg-gray-300','text-black')
-   thrivingFilterBtn.classList.add('bg-gray-300', 'text-black')
-   strugglingFilterBtn.classList.add('bg-gray-300','text-black')
+    allFilterBtn.classList.add('bg-gray-300', 'text-black')
+    thrivingFilterBtn.classList.add('bg-gray-300', 'text-black')
+    strugglingFilterBtn.classList.add('bg-gray-300', 'text-black')
 
-   const selected = document.getElementById(id);
-   selected.classList.remove('bg-gray-300','text-black')
-   selected.classList.add('bg-black','text-white')
+    const selected = document.getElementById(id);
+    currentStatus = id
+
+    selected.classList.remove('bg-gray-300', 'text-black')
+    selected.classList.add('bg-black', 'text-white')
+
+    if(id == 'thriving-filter-btn'){
+        allCardSection.classList.add('hidden')
+        filteredSection.classList.remove('hidden')
+        renderThriving()
+    }
+    else if(id == 'all-filter-btn'){
+        allCardSection.classList.remove('hidden')
+        filteredSection.classList.add('hidden')
+    }
+    else if (id == 'struggling-filter-btn'){
+        allCardSection.classList.add('hidden')
+        filteredSection.classList.remove('hidden')
+        renderStruggling()
+    }
 }
 
-mainContainer.addEventListener('click', function(event){
+mainContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('thriving-btn')) {
+        const ParentNode = event.target.parentNode.parentNode;
+        const plantName = ParentNode.querySelector('.plantName').innerText;
+        const latinName = ParentNode.querySelector('.latinName').innerText;
+        const light = ParentNode.querySelector('.light').innerText;
+        const water = ParentNode.querySelector('.water').innerText;
+        const status = ParentNode.querySelector('.status').innerText;
+        const notes = ParentNode.querySelector('.notes').innerText;
 
-    const ParentNode = event.target.parentNode.parentNode;
-    const plantName = ParentNode.querySelector('.plantName').innerText;
-    const latinName = ParentNode.querySelector('.latinName').innerText;
-    const light = ParentNode.querySelector('.light').innerText;
-    const water = ParentNode.querySelector('.water').innerText;
-    const status = ParentNode.querySelector('.status').innerText;
-    const notes = ParentNode.querySelector('.notes').innerText;
+         ParentNode.querySelector('.status').innerText  = 'Thrive';
 
-    const CardInfo = {
-        plantName, 
-        latinName,
-        light,
-        water,
-        status,
-        notes
+        const CardInfo = {
+            plantName,
+            latinName,
+            light,
+            water,
+            status:'Thrive',
+            notes
+        }
+
+
+        //validation for same card no need push in array
+        const plantExist = thrivlingCountList.find(item => item.plantName == CardInfo.plantName)
+       
+
+        if (!plantExist) {
+            thrivlingCountList.push(CardInfo)
+        }
+
+         strugglingCountList = strugglingCountList.filter(item=>item.plantName != CardInfo.plantName)
+
+         calculateTotalCount() 
+
+         if(currentStatus == "struggling-filter-btn"){
+            renderStruggling()
+        }
+        
+       
+      
     }
+    else if (event.target.classList.contains('struggling-btn')) {
+        const ParentNode = event.target.parentNode.parentNode;
+        const plantName = ParentNode.querySelector('.plantName').innerText;
+        const latinName = ParentNode.querySelector('.latinName').innerText;
+        const light = ParentNode.querySelector('.light').innerText;
+        const water = ParentNode.querySelector('.water').innerText;
+        const status = ParentNode.querySelector('.status').innerText;
+        const notes = ParentNode.querySelector('.notes').innerText;
 
-    // console.log(CardInfo);
+         ParentNode.querySelector('.status').innerText  = 'Struggle';
 
-    //validation for same card no need push in array
-    const plantExist = strugglingCountList.find(item=> item.plantName == CardInfo.plantName)
-    if(!plantExist){
-        strugglingCountList.push(CardInfo)
+        const CardInfo = {
+            plantName,
+            latinName,
+            light,
+            water,
+            status:'Struggle',
+            notes
+        }
+
+
+        //validation for same card no need push in array
+        const plantExist = strugglingCountList.find(item => item.plantName == CardInfo.plantName)
+       
+
+        if (!plantExist) {
+            strugglingCountList.push(CardInfo)
+        }
+
+        thrivlingCountList = thrivlingCountList.filter(item=>item.plantName != CardInfo.plantName)
+
+        if(currentStatus == "thriving-filter-btn"){
+            renderThriving()
+        }
+        calculateTotalCount() 
     }
-    renderThriving()
-    // console.log(strugglingCountList);    
 })
 
-function renderThriving(){
+function renderThriving() {
     filteredSection.innerHTML = ''
-    for(let thrive of thrivlingCountList){
+
+    for (let thrive of thrivlingCountList) {
+        // console.log(thrive);
         let div = document.createElement('div');
         div.className = 'card flex justify-between border border-gray-300 p-10 rounded'
         div.innerHTML = `
          <div class="space-y-4">
                     <div>
-                        <p class="plantName text-3xl">Plant Name 1</p>
-                        <p class="latinName">Latin Name</p>
+                        <p class="plantName text-3xl">${thrive.plantName}</p>
+                        <p class="latinName">${thrive.latinName}</p>
                     </div>
 
                     <div class="flex gap-5">
-                        <p class="light bg-gray-200 py-1 px-3">Bright Indicate</p>
-                        <p class="water bg-gray-200 py-1 px-3">Weekly</p>
+                        <p class="light bg-gray-200 py-1 px-3">${thrive.light}</p>
+                        <p class="water bg-gray-200 py-1 px-3">${thrive.water}</p>
                     </div>
 
-                    <p class="status">Not Applicable</p>
+                    <p class="status">${thrive.status}</p>
                     <p class="notes">New leaf unforling by the east window.</p>
 
                     <div class="flex gap-5">
@@ -97,5 +166,40 @@ function renderThriving(){
                     <button class="delete-btn bg-red-200 text-red-600 py-2 px-4">Delete</button>
                 </div>
             `
-    }    
-}     
+            filteredSection.appendChild(div)
+    }
+}      
+function renderStruggling() {
+    filteredSection.innerHTML = ''
+
+    for (let struggle of strugglingCountList) {
+        // console.log(thrive);
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between border border-gray-300 p-10 rounded'
+        div.innerHTML = `
+         <div class="space-y-4">
+                    <div>
+                        <p class="plantName text-3xl">${struggle.plantName}</p>
+                        <p class="latinName">${struggle.latinName}</p>
+                    </div>
+
+                    <div class="flex gap-5">
+                        <p class="light bg-gray-200 py-1 px-3">${struggle.light}</p>
+                        <p class="water bg-gray-200 py-1 px-3">${struggle.water}</p>
+                    </div>
+
+                    <p class="status">${struggle.status}</p>
+                    <p class="notes">New leaf unforling by the east window.</p>
+
+                    <div class="flex gap-5">
+                        <button class="thriving-btn bg-green-200 px-4 py-2">Thrive</button>
+                        <button class="struggling-btn bg-red-200 px-4 py-2">Struggle</button>
+                    </div>
+                </div>
+                <div>
+                    <button class="delete-btn bg-red-200 text-red-600 py-2 px-4">Delete</button>
+                </div>
+            `
+            filteredSection.appendChild(div)
+    }
+}      
